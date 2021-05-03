d60179fb61da526cb190521e7f0ffd8ceade818b
naveenmani21 <naveen.chakaravarathi@gmail.com>
2021-05-03 16:45:46 +0530

    Push to branch

--- src/org/sampleee/Git.java
+++ src/org/sampleee/Git.java
@@ -3,5 +3,7 @@ package org.sampleee;
 public class Git {
 public static void main(String[] args) {
 	System.out.println("Sql Preparetionnn data");
+	System.out.println("qqwertyuiop");
+	System.out.println("sdfghjkl.;/dertyuiop1234567890");
 }
 }
